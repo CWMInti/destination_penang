bf2d132f6f4ce8dd3f947cb4985baf1bb19aa887
CWMInti <82583267+CWMInti@users.noreply.github.com>
2023-06-22 20:21:21 +0800

    Update review_handler.php

--- destination_penang/php/review_handler.php
+++ destination_penang/php/review_handler.php
@@ -6,6 +6,7 @@
 	$form_nickname = $_POST['form_nickname'];
 	$form_placename = $_POST['form_placename'];
 	$form_placeaddress = $_POST['form_placeaddress'];
+	$form_placerating = $_POST['form_placerating'];
 	$form_placereview = $_POST['form_placereview'];
 
 	// Create connection
@@ -25,27 +26,31 @@
 		} else if ($form_placeaddress == '') {
 			echo "<h1><center>Please enter the place address.</center></h1>";
 			echo "<h2><center><a href='../places-reviews.php'>Check your review again</a><center></h2>";
-			} else if ($form_placereview == '') {
-					echo "<h1><center>Your review is empty....</center></h1>";
-					echo "<h2><center><a href='../places-reviews.php'>Check your review again</a><center></h2>";
-				} else if (!$form_placereview == '') {
-					$stmt = $conn->prepare("INSERT INTO places_reviews (nickname, place_name, place_address, place_review) VALUES (?, ?, ?, ?)");
-					$stmt->bind_param ("ssss", $form_nickname, $form_placename, $form_placeaddress, $form_placereview);
+			} else if ($form_placerating == '0 out of 5') {
+				echo "<h1><center>Please select a rating</center></h1>";
+				echo "<h2><center><a href='../places-reviews.php'>Check your review again</a><center></h2>";
+				} else if ($form_placereview == '') {
+						echo "<h1><center>Your review is empty....</center></h1>";
+						echo "<h2><center><a href='../places-reviews.php'>Check your review again</a><center></h2>";
+					} else if (!$form_placereview == '') {
+						$stmt = $conn->prepare("INSERT INTO places_reviews (nickname, place_name, place_address, place_rating, place_review) VALUES (?, ?, ?, ?, ?)");
+						$stmt->bind_param ("sssss", $form_nickname, $form_placename, $form_placeaddress, $form_placerating, $form_placereview);
 
-					if ($stmt->execute()) {
-						// Display a success message and the review details
-						?>
-						<h1><center>Thanks for your Review!</center></h1>
-						<h2><center>Your review:</center></h2>
-						<h3><center>Nickname: <?php echo $form_nickname; ?></center></h3>
-						<h3><center>Place: <?php echo $form_placename; ?></center></h3>
-						<h3><center>Address: <?php echo $form_placeaddress; ?></center></h3>
-						<h3><center><?php echo $form_placereview; ?></center></h3>
-						<h2><center><a href='../places-reviews.php'>Back to Places Reviews</a><center></h2>
-						<?php
-					} else {
-						// Display an error message
-						echo "Error: " . $stmt->error;
+						if ($stmt->execute()) {
+							// Display a success message and the review details
+							?>
+							<h1><center>Thanks for your Review!</center></h1>
+							<h2><center>Your review:</center></h2>
+							<h3><center>Nickname: <?php echo $form_nickname; ?></center></h3>
+							<h3><center>Place: <?php echo $form_placename; ?></center></h3>
+							<h3><center>Address: <?php echo $form_placeaddress; ?></center></h3>
+							<h3><center>Rating: <?php echo $form_placerating; ?></center></h3>
+							<h3><center><?php echo $form_placereview; ?></center></h3>
+							<h2><center><a href='../places-reviews.php'>Back to Places Reviews</a><center></h2>
+							<?php
+						} else {
+							// Display an error message
+							echo "Error: " . $stmt->error;
+						}
 					}
-				}
 ?>  
