da498390e765fb168b25088a75f0625e9c459916
CWMInti <82583267+CWMInti@users.noreply.github.com>
2023-06-23 22:06:08 +0800

    Update review_deletion.php

--- destination_penang/admin/review_deletion.php
+++ destination_penang/admin/review_deletion.php
@@ -13,23 +13,36 @@
 	  die("Connection failed: " . mysqli_connect_error());
 	}
 	
-    if ($review_id == '') {
+    $stmt1 = $conn->prepare("SELECT * FROM places_reviews where id = ?");
+	$stmt1->bind_param ("s", $review_id);
+	$stmt1->execute ();
+	$result = $stmt1->get_result ();
+	$row = $result->fetch_array ();
+	$count = mysqli_num_rows($result);
+	  
+	if ($review_id == '') {
         ?>
-        <h1><center>No ID was entered</center></h1>
+        <h2><center>No ID was entered</center></h2>
         <h2><center><a href='admin_page.php'>Please enter the ID for review deletion</a><center></h2>
         <?php
-    } else if (!$review_id == '') {
-        $sql = "";
-        $stmt = $conn->prepare("DELETE FROM places_reviews WHERE id = ?");
-		$stmt->bind_param ("s", $review_id);
-        
-        if ($stmt->execute()) {
-            ?>
-            <h3><center>Review ID: <?php echo $review_id; ?> has been deleted</center></h3>
-            <h2><center><a href='admin_page.php'>Back to Reviews Management</a><center></h2>
-            <?php
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-    }
+    } else if($count == 1){  
+        if (!$review_id == '') {
+           $stmt = $conn->prepare("DELETE FROM places_reviews WHERE id = ?");
+           $stmt->bind_param ("s", $review_id);
+           
+           if ($stmt->execute()) {
+               ?>
+               <h2><center>Review ID: <?php echo $review_id; ?> has been deleted</center></h2>
+               <h2><center><a href='admin_page.php'>Back to Reviews Management</a><center></h2>
+               <?php
+           } else {
+               echo "Error: " . $stmt->error;
+           }
+       }
+   } else {
+       ?>
+       <h2><center>Review ID: <?php echo $review_id; ?> doesn't exist in the database</center></h2>
+       <h2><center><a href='admin_page.php'>Back to Reviews Management</a><center></h2>
+       <?php
+   }
 ?>  
